7b76b6b52929daa65de3a7a04f66e5ce2013ce55
yabadadu <ck.park@outlook.kr>
2021-08-27 11:38:26 +0900

    초기 커밋.

---
+++ MyModules/SingleTon/SingleTon.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyModules.SingleTon
+{
+    public class UserInfoSingleton
+    {
+        private string userid;
+        private string username;
+        private static UserInfoSingleton instance = null;
+
+        private UserInfoSingleton()
+        {
+        }
+
+        // 변수를 이용한 방법
+        public static UserInfoSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new UserInfoSingleton();
+                }
+                return instance;
+            }
+        }
+
+        // 변수를 이용한 방법
+        public static UserInfoSingleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new UserInfoSingleton();
+            }
+            return instance;
+        }
+
+        public string Userid
+        {
+            get
+            {
+                return this.userid;
+            }
+            set
+            {
+                this.userid = value;
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                return this.username;
+            }
+            set
+            {
+                this.username = value;
+            }
+        }
+    }
+}
