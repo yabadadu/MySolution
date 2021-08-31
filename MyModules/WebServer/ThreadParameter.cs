78e5411c7aa4394d1708ad486aa09c19834f6f9d
yabadadu <88810408+yabadadu@users.noreply.github.com>
2021-08-31 16:23:00 +0900

    Add files via upload

---
+++ MyModules/WebServer/ThreadParameter.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Threading;
+
+namespace MyModules.WebServer
+{
+    /// <summary>
+    /// 스레드 매개 변수
+    /// </summary>
+    public class ThreadParameter
+    {
+        //////////////////////////////////////////////////////////////////////////////////////////////////// Property
+        ////////////////////////////////////////////////////////////////////////////////////////// Public
+
+        #region 컨텍스트 - Context
+
+        /// <summary>
+        /// 컨텍스트
+        /// </summary>
+        public HttpListenerContext Context { get; set; }
+
+        #endregion
+        #region 스레드 - Thread
+
+        /// <summary>
+        /// 스레드
+        /// </summary>
+        public Thread Thread { get; set; }
+
+        #endregion
+    }
+}
